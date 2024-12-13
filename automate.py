bdfb60ee43dac9d60a280a90e5441b8a621fc0ba
Adarshadi88 <69230780+Adarshadi88@users.noreply.github.com>
2024-12-13 10:19:17 +0530

    time

--- automate.py
+++ automate.py
@@ -7,7 +7,7 @@ import time
 chrome_driver = webdriver.Chrome()
 # opening a web url 
 chrome_driver.get("https://rahulshettyacademy.com/angularpractice/")
-time.sleep(3)
+time.sleep(6)
 
 # printing title 
 print("page title : ",chrome_driver.title)
